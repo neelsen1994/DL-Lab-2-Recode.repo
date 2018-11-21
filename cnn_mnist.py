c82826e85c465fb57a76e113bb1142268fb16548
neilsen1994 <debayan.sen1994@gmail.com>
2018-11-21 01:58:52 +0100

    Add files via upload

---
+++ cnn_mnist.py
@@ -0,0 +1,226 @@
+from __future__ import print_function
+
+import argparse
+import gzip
+import json
+import os
+import pickle
+import tensorflow as tf
+import numpy as np
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+
+def one_hot(labels):
+    """this creates a one hot encoding from a flat vector:
+    i.e. given y = [0,2,1]
+     it creates y_one_hot = [[1,0,0], [0,0,1], [0,1,0]]
+    """
+    classes = np.unique(labels)
+    n_classes = classes.size
+    one_hot_labels = np.zeros(labels.shape + (n_classes,))
+    for c in classes:
+        one_hot_labels[labels == c, c] = 1
+    return one_hot_labels
+
+
+def mnist(datasets_dir='./data'):
+    if not os.path.exists(datasets_dir):
+        os.mkdir(datasets_dir)
+    data_file = os.path.join(datasets_dir, 'mnist.pkl.gz')
+    if not os.path.exists(data_file):
+        print('... downloading MNIST from the web')
+        try:
+            import urllib
+            urllib.urlretrieve('http://google.com')
+        except AttributeError:
+            import urllib.request as urllib
+        url = 'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
+        urllib.urlretrieve(url, data_file)
+
+    print('... loading data')
+    # Load the dataset
+    f = gzip.open(data_file, 'rb')
+    try:
+        train_set, valid_set, test_set = pickle.load(f, encoding="latin1")
+    except TypeError:
+        train_set, valid_set, test_set = pickle.load(f)
+    f.close()
+
+    test_x, test_y = test_set
+    test_x = test_x.astype('float32')
+    test_x = test_x.astype('float32').reshape(test_x.shape[0], 28, 28, 1)
+    test_y = test_y.astype('int32')
+    valid_x, valid_y = valid_set
+    valid_x = valid_x.astype('float32')
+    valid_x = valid_x.astype('float32').reshape(valid_x.shape[0], 28, 28, 1)
+    valid_y = valid_y.astype('int32')
+    train_x, train_y = train_set
+    train_x = train_x.astype('float32').reshape(train_x.shape[0], 28, 28, 1)
+    train_y = train_y.astype('int32')
+    print('... done loading data')
+    return train_x, one_hot(train_y), valid_x, one_hot(valid_y), test_x, one_hot(test_y)
+
+def conv2d(x, W, b, strides=1):
+    # Conv2D wrapper, with bias and relu activation
+    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
+    x = tf.nn.bias_add(x, b)
+    return tf.nn.relu(x) 
+
+def maxpool2d(x, k=2):
+    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],padding='SAME')
+
+def conv_net(x, weights, biases):  
+
+    # here we call the conv2d function we had defined above and pass the input image x, weights wc1 and bias bc1.
+    conv1 = conv2d(x, weights['wc1'], biases['bc1'])
+    # Max Pooling (down-sampling), this chooses the max value from a 2*2 matrix window and outputs a 14*14 matrix.
+    conv1 = maxpool2d(conv1, k=2)
+
+    # Convolution Layer
+    # here we call the conv2d function we had defined above and pass the input image x, weights wc2 and bias bc2.
+    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
+    # Max Pooling (down-sampling), this chooses the max value from a 2*2 matrix window and outputs a 7*7 matrix.
+    conv2 = maxpool2d(conv2, k=2)
+
+    conv3 = conv2d(conv2, weights['wc3'], biases['bc3'])
+    # Max Pooling (down-sampling), this chooses the max value from a 2*2 matrix window and outputs a 4*4.
+    conv3 = maxpool2d(conv3, k=2)
+
+
+    # Fully connected layer
+    # Reshape conv2 output to fit fully connected layer input
+    fc1 = tf.reshape(conv3, [-1, weights['wd1'].get_shape().as_list()[0]])
+    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
+    fc1 = tf.nn.relu(fc1)
+    # Output, class prediction
+    # finally we multiply the fully connected layer with the weights and add a bias term. 
+    out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
+    return out
+
+def train_and_validate(x_train, y_train, x_valid, y_valid, num_epochs, lr, num_filters, batch_size):
+    # TODO: train and validate your convolutional neural networks with the provided data and hyperparameters
+    learning_curve = []
+    x = tf.placeholder("float", [None, 28,28,1])
+    y = tf.placeholder("float", [None, 10])
+    weights = {
+        'wc1': tf.get_variable('W0', shape=(3,3,1,32), initializer=tf.contrib.layers.xavier_initializer()), 
+        'wc2': tf.get_variable('W1', shape=(3,3,32,64), initializer=tf.contrib.layers.xavier_initializer()), 
+        'wc3': tf.get_variable('W2', shape=(3,3,64,128), initializer=tf.contrib.layers.xavier_initializer()), 
+        'wd1': tf.get_variable('W3', shape=(4*4*128,128), initializer=tf.contrib.layers.xavier_initializer()), 
+        'out': tf.get_variable('W6', shape=(128,10), initializer=tf.contrib.layers.xavier_initializer()), 
+    }
+    biases = {
+        'bc1': tf.get_variable('B0', shape=(32), initializer=tf.contrib.layers.xavier_initializer()),
+        'bc2': tf.get_variable('B1', shape=(64), initializer=tf.contrib.layers.xavier_initializer()),
+        'bc3': tf.get_variable('B2', shape=(128), initializer=tf.contrib.layers.xavier_initializer()),
+        'bd1': tf.get_variable('B3', shape=(128), initializer=tf.contrib.layers.xavier_initializer()),
+        'out': tf.get_variable('B4', shape=(10), initializer=tf.contrib.layers.xavier_initializer()),
+    }
+    pred = conv_net(x, weights, biases)
+
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+
+    optimizer = tf.train.AdamOptimizer(learning_rate=lr).minimize(cost)
+    
+        #Here you check whether the index of the maximum value of the predicted image is equal to the actual labelled image. and both will be a column vector.
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+
+#calculate accuracy across all the given images and average them out. 
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    init = tf.global_variables_initializer()
+    
+    with tf.Session() as sess:
+        sess.run(init) 
+        train_loss = []
+        
+        train_accuracy = []
+        
+        summary_writer = tf.summary.FileWriter('./Output', sess.graph)
+        for i in range(num_epochs):
+            for batch in range(len(x_train)//batch_size):
+                batch_x = x_train[batch*batch_size:min((batch+1)*batch_size,len(x_train))]
+                batch_y = y_train[batch*batch_size:min((batch+1)*batch_size,len(y_train))]    
+                # Run optimization op (backprop).
+                # Calculate batch loss and accuracy
+                opt = sess.run(optimizer, feed_dict={x: batch_x,
+                                                              y: batch_y})
+                loss, acc = sess.run([cost, accuracy], feed_dict={x: batch_x,
+                                                              y: batch_y})
+            print("Iter " + str(i) + ", Loss= " + \
+                      "{:.6f}".format(loss) + ", Training Accuracy= " + \
+                      "{:.5f}".format(acc))
+            print("Optimization Finished!")
+            train_loss.append(loss)
+            train_accuracy.append(acc)
+            # Calculate accuracy for all 10000 mnist test images
+            lc = dict()
+            lc["accuracy"] = acc
+            lc["loss"] = loss
+            lc["global_step"] = i
+            learning_curve.append(lc)
+            summary_writer.close()
+        
+    return learning_curve, sess  # TODO: Return the validation error after each epoch (i.e learning curve) and your model
+
+
+def test(x_test, y_test, model):
+    # TODO: test your network here by evaluating it on the test data
+    test_loss = []
+    test_accuracy = []
+    test_acc,valid_loss = model.run([accuracy,cost], feed_dict={x: x_test,y : y_test})
+    
+    #test_loss.append(valid_loss)
+    
+    #test_accuracy.append(test_acc)
+    print("Testing Accuracy:","{:.5f}".format(test_acc))
+  
+    return valid_loss
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output_path", default="./", type=str, nargs="?",
+                        help="Path where the results will be stored")
+    parser.add_argument("--input_path", default="./", type=str, nargs="?",
+                        help="Path where the data is located. If the data is not available it will be downloaded first")
+    parser.add_argument("--learning_rate", default=1e-3, type=float, nargs="?", help="Learning rate for SGD")
+    parser.add_argument("--num_filters", default=32, type=int, nargs="?",
+                        help="The number of filters for each convolution layer")
+    parser.add_argument("--batch_size", default=128, type=int, nargs="?", help="Batch size for SGD")
+    parser.add_argument("--epochs", default=12, type=int, nargs="?",
+                        help="Determines how many epochs the network will be trained")
+    parser.add_argument("--run_id", default=0, type=int, nargs="?",
+                        help="Helps to identify different runs of an experiments")
+
+    args = parser.parse_args()
+
+    # hyperparameters
+    lr = args.learning_rate
+    num_filters = args.num_filters
+    batch_size = args.batch_size
+    epochs = args.epochs
+
+    # train and test convolutional neural network
+    x_train, y_train, x_valid, y_valid, x_test, y_test = mnist(args.input_path)
+
+    learning_curve, model = train_and_validate(x_train, y_train, x_valid, y_valid, epochs, lr, num_filters, batch_size)
+
+    test_error = test(x_test, y_test, model)
+
+    # save results in a dictionary and write them into a .json file
+    results = dict()
+    results["lr"] = lr
+    results["num_filters"] = num_filters
+    results["batch_size"] = batch_size
+    results["learning_curve"] = learning_curve
+    results["test_error"] = test_error
+
+    path = os.path.join(args.output_path, "results")
+    os.makedirs(path, exist_ok=True)
+
+    fname = os.path.join(path, "results_run_%d.json" % args.run_id)
+
+    fh = open(fname, "w")
+    json.dump(results, fh)
+    fh.close()
